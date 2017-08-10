f2875aa98a825ae61144b4805b702cbdbc1a8834
petersez <petersez@EPPETERSEZW530>
2017-08-10 12:02:17 -0500

    Add project files.

---
+++ Team3GroupProject/Team3GroupProject.cpp
@@ -0,0 +1,28 @@
+//	CSC301 Group Assignment
+//	
+//  Group Members: Katie Delano, Riley Finn, Mohamed Mohamed, Zach Petersen
+//  Advanced Calculator
+
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+/*
+General Program Outline
+Take user input
+Validate
+Perform mathematical operations
+Return the solution
+*/
+
+int main()
+{
+	//This is a placeholder
+}
+
+double mathFunction(double value1, string op, double value2)
+{
+	//This is a placeholder
+	return 0;
+}
