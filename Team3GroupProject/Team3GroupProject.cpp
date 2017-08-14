271fec8aae564fd04e683756264e41549ef2d2a2
ZachPetersen1 <30872949+ZachPetersen1@users.noreply.github.com>
2017-08-13 20:44:37 -0500

    Update Team3GroupProject.cpp
    added outline description

--- Team3GroupProject/Team3GroupProject.cpp
+++ Team3GroupProject/Team3GroupProject.cpp
@@ -18,6 +18,22 @@ Return the solution
 
 int main()
 {
+	// Prompt user for equation
+	
+	// Parse each number/operator into an array object
+	
+	// Iterate through array looking for operators
+	
+	// When an operator is found, pass it and it's neighboring elements to the mathFunction	
+	
+	// Replace 3 array elements with the method result
+	
+	// Repeat until no operators exist and array length is 1
+	
+	// Print the equation and solution for the user
+	
+	
+	
 	cout << "Testing 1,2,3" << endl;
 	cout << "Testing 4,5,6" << endl;
 	//This is a placeholder
